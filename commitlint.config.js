3eaa13234c8256379d5510f757bc914d21a97dbd
Elias Knodel <elias.knodel@gmail.com>
2022-07-30 01:56:18 +0200

    chore: update community health files
    Refs: #52

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,39 +1,8 @@
 // Configuration of commitlint to check commit message guidelines
 module.exports = {
-    parserPreset: "conventional-changelog-conventionalcommits",
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: 'conventional-changelog-conventionalcommits',
     rules: {
-      "subject-max-length": [2, "always", 50],
-      "subject-case": [
-        2,
-        "never",
-        ["sentence-case", "start-case"]
-      ],
-      "subject-empty": [2, "never"],
-      "subject-full-stop": [2, "never", "."],
-      "type-case": [2, "always", "lower-case"],
-      "type-empty": [2, "never"],
-      "type-enum": [
-        2,
-        "always",
-        [
-          "feat",
-          "fix",
-          "perf",
-          "refactor",
-          "cs",
-          "test",
-          "build",
-          "ci",
-          "docs",
-          "changelog",
-          "bump"
-        ]
-      ],
-      "scope-empty": [2, "always"],
-      "header-max-length": [2, "always", 75],
-      "body-leading-blank": [1, "always"],
-      "body-max-line-length": [2, "always", 75],
-      "footer-leading-blank": [1, "always"],
-      "footer-max-line-length": [2, "always", 75]
+        'subject-max-length': [2, 'always', 50]
     }
-  };
+};
